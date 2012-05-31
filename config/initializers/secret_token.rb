114b8af54134940d4931892854a2d9e299e64c3e
Sam Pierson <sam@sampierson.com>
2012-05-30 20:32:20 -0700

    rails new sts_com --skip-test-unit --database=postgresql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StsCom::Application.config.secret_token = '2d18a45ca6812294b45afb41c54df7c4e119de4bad45f5beb917cffec165f0a2ca8799df9e0982958a0a2135b4489960ef67ab40bda4689190b124cc2cdf62bf'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+StsCom::Application.config.session_store :cookie_store, key: '_sts_com_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# StsCom::Application.config.session_store :active_record_store
